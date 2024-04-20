d4e74b82b121da281e465a7381fda0d73d306f28
Lais_Nuto_Rossman <laisnuto@gmail.com>
2024-04-20 07:32:55 -0300

    fix files

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,6 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center">
       <HomeCoverSection blogs={allBlogs} />
-      <FeaturedPosts blogs={allBlogs} />
       <RecentPosts blogs={allBlogs} />
 
 
