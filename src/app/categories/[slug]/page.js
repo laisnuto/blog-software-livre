d3b1f65b9df3e34867aa9a6113c1e46c64c884c6
Lais_Nuto_Rossman <laisnuto@gmail.com>
2024-06-25 01:49:30 -0300

    s

--- src/app/categories/[slug]/page.js
+++ src/app/categories/[slug]/page.js
@@ -59,9 +59,6 @@ const blogs = allBlogs.filter(blog => {
     <article className="mt-12 flex flex-col text-dark dark:text-light">
       <div className=" px-5 sm:px-10  md:px-24  sxl:px-32 flex flex-col">
         <h1 className="mt-6 font-semibold text-2xl md:text-4xl lg:text-5xl">#{params.slug}</h1>
-        <span className="mt-2 inline-block">
-          Discover more categories and expand your knowledge!
-        </span>
       </div>
       <Categories categories={allCategories} currentSlug={params.slug} />
 
